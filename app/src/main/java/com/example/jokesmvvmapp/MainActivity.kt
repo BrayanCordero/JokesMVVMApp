583b0b9a0181864a153dd302039855a29e337b3e
BrayanCordero <corderobrayan@yaho.com>
2022-06-25 15:09:35 -0400

    navigation is workin

--- app/src/main/java/com/example/jokesmvvmapp/MainActivity.kt
+++ app/src/main/java/com/example/jokesmvvmapp/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.jokesmvvmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.jokesmvvmapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -17,6 +22,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navigationBar
+        val navController = findNavController(R.id.nav_jokes_container)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_change_name_fragment, R.id.navigation_never_ending_fragment, R.id.navigation_random_joke_fragment
+            )
+        )
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        navView.setupWithNavController(navController)
 
 
     }
